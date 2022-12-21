37119abd79631593910f86a8c008a675a24f421d
SaiKiran-18 <111628140+SaiKiran-18@users.noreply.github.com>
2022-12-21 17:21:24 +0530

    Assignment_1

---
+++ Assinemnt_1/src/com/prime/java/Prime.java
@@ -0,0 +1,23 @@
+package com.prime.java;
+
+	public class Prime {
+		public boolean  Check(int n) {
+			for(int i=2;i<n;i++) {
+				if(n%i==0) {
+					return false;
+				}
+			}
+			return true;	
+		}
+	public static void main(String args[]) {
+		Prime prime=new Prime();
+		if(prime.Check(673)) {
+			System.out.println("prime");
+		}
+		else {
+			System.out.println("Not prime");
+		}
+	}
+	}
+
+
